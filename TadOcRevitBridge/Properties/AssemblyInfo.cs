cb23b3e7b47e9910582e88dc3c9be1fbc82f72ef
tad-orchestrator <emnss26@gmail.com>
2026-04-23 22:03:35 -0600

    Laste updates

--- TadOcRevitBridge/Properties/AssemblyInfo.cs
+++ TadOcRevitBridge/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TadOcRevitBridge")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCopyright("Copyright © c 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
